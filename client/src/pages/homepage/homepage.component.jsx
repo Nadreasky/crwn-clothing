d76b71403c33d90b64295220b71f3b59c41ca4b1
qhuynh <qhuynh@bocasay.com>
2020-04-13 23:47:34 +0700

    update app to pwa compliant, added serviceWorker, https, update manifest

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Profiler } from 'react';
 
 import './homepage.styles.scss';
 
@@ -6,7 +6,15 @@ import Directory from '../../components/directory/directory.component';
 
 const HomePage = () => (
   <div className='homepage'>
-    <Directory />
+    <Profiler id='Directory' onRender={(id, phase, actualDuration) => {
+      console.log({
+        id,
+        phase,
+        actualDuration
+      });
+    }}>
+      <Directory />
+    </Profiler>
   </div>
 );
 
